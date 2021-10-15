3816fdb4644963a29b443d64e9520fd99fae8c2b
ash-development <ash@schnoor.email>
2021-10-15 12:19:47 +0000

    base code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS]
+});
 
+client.on("ready", () => {
+  console.log(chalk.magenta("Bot has loaded up."));
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const { commandName } = interaction;
+  if (commandName === 'ping') {
+    await interaction.reply('pong!');
+  }
+});
+
+client.login("SuperSecretBotTokenHere");
